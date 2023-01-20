583bbc4e9b4d7d1240e63c8a1486ddbbd0911e48
SuperSpiritEagle <118557578+SuperSpiritEagle@users.noreply.github.com>
2023-01-20 13:40:13 +0500

    init

---
+++ PasswordProtectedProgram/PasswordProtectedProgram/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PasswordProtectedProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretMessage = "secret Message";
+            string pincode;
+            string password = "123";
+            int numberAttempts = 3;
+
+
+            for (int i = numberAttempts;i>0;i--)
+            {
+                Console.WriteLine("Введите пароль чтобы прочитать тайное сообщение");
+                Console.WriteLine($"количество попыток {i}");
+
+                pincode = Console.ReadLine();
+
+                if (pincode == password)
+                {
+                    Console.WriteLine(secretMessage);
+                    break;
+                }
+            }
+        }
+    }
+}
